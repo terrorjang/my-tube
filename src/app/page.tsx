8c82d7573fbc8b6819f2934e02418001694da967
youngseok.jang <ys123.jang@gmail.com>
2025-05-22 22:22:09 +0900

    02 Basic Layout

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <div className="font-bold text-rose-500">
-      <Button variant={"destructive"}>Click Me!</Button>
-    </div>
-  );
-}
---
+++ src/modules/home/ui/layouts/home-layout.tsx
@@ -0,0 +1,25 @@
+import { SidebarProvider } from '@/components/ui/sidebar';
+import { HomeNavBar } from '../components/home-navbar';
+import { HomeSidebar } from '../components/hom-sidebar';
+
+interface HomeLayoutProps {
+  children: React.ReactNode;
+}
+
+export const HomeLayout = ({
+  children,
+}: HomeLayoutProps) => {
+  return (
+    <SidebarProvider>
+      <div className="w-full">
+        <HomeNavBar />
+        <div className="flex min-h-screen pt-[4rem]">
+          <HomeSidebar />
+          <main className="flex-1 overflow-y-auto">
+            {children}
+          </main>
+        </div>
+      </div>
+    </SidebarProvider>
+  );
+};
